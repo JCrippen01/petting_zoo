6ba4913d0e31f91df8b0377bea68545445ab9241
Justin <justin.crippen@gmail.com>
2021-10-21 09:22:51 -0500

    first commit

---
+++ animals.py
@@ -0,0 +1,213 @@
+from datetime import date
+
+
+class Axolotl:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Mantis_Shrimp:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Llama:
+
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = True
+        self.date_added = date.today()
+
+class Dog:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = True
+        self.date_added = date.today()
+
+
+class Wombat:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = True
+        self.date_added = date.today()
+
+class Shoebill:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Glass_Frog:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+
+class Flightless_Cormorant:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Fainting_Goat:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = True
+        self.date_added = date.today()
+
+class Markhor:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+
+class Okapi:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Aye_Aye:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Parrotfish:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Lyrebird:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+
+class Sun_Bear:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Black_Mamba:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+class Banded_Krait:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.isfreindly = False
+        self.date_added = date.today()
+
+
+class  Inland_Taipan:
+    def __init__(self, name, species):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.isfreindly =  True
+        self.date_added = date.today()
+        
+Axel = Axolotl("Axel", "Axolotl")
+Monty = Mantis_Shrimp("Monty", "Mantis_Shrimp")
+Louise = Llama("Louise", "Llama")
+Dan = Dog("Dan", "Dog")
+Willie = Wombat("Willie", "Wombat")
+Sally = Shoebill("Sally", "Shoebill")
+Blondie = Glass_Frog("Blondie", "Glass Frog")
+Fred = Flightless_Cormorant("Fred", "Flightless Cormorant")
+Frank = Fainting_Goat("Frank", "Farting Goat")
+Mark = Markhor("Mark", "Markhor")
+Oscar = Okapi("Oscar", "Okapi")
+Aya = Aye_Aye("Aya", "Aye-Aye")
+Perro = Parrotfish("Perro", "Parrotfish")
+Liar = Lyrebird("Liar", "Lyrebird")
+Sunny = Sun_Bear("Sonny", "Sun Bear")
+Hernando = Black_Mamba("Hernando", "Black Mamaba")
+Jon = Banded_Krait("Jon", "Banded_Krait")
+Herbie = Inland_Taipan("Herbie", "Inland Taipan")
+print(Herbie)
+
+
+
+
+
